8b308511687d250df1693f288657cd77b84a9a30
Kitty Elliott <kitty@glitchcat.space>
2020-08-03 22:53:39 -0700

    added client pages for login and account creation

---
+++ reversi/public_html/login.js
@@ -0,0 +1,38 @@
+/* Kitty Elliott
+ * CSC 337
+ * Final: Reversi
+ * 	script for login page of Reversi app
+ */
+
+function formSubmitHandler(event) {
+	event.preventDefault();
+	$.post({
+		url: this.action,
+		data: $(this).serialize(),
+		error: e => $("<p/>")
+			.text(e.responseText)
+			.appendTo("#notifArea"),
+		success: _ => window.location.href = "/home.html",
+	});
+}
+
+function unauthorizedKeyPresent() {
+	return window.location.search
+		.substring(1)
+		.split("&")
+		.includes("unauthorized");
+}
+
+function main() {
+	$("#loginForm").submit(formSubmitHandler);
+	$("#regForm").submit(formSubmitHandler);
+	$("#notifArea").click(function() { $(this).empty() })
+
+	if (unauthorizedKeyPresent()) {
+		$("<p/>")
+			.text("Unauthorized! Your session may have expired")
+			.appendTo("#notifArea");
+	}
+}
+
+$(main);
