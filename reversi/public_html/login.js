6577bf9d7efa95f5247293260fb0a02ee9643774
Kitty Elliott <kitty@glitchcat.space>
2020-08-06 18:51:23 -0700

    added guest game link to login page

--- reversi/public_html/login.js
+++ reversi/public_html/login.js
@@ -24,16 +24,27 @@ function unauthorizedKeyPresent() {
 }
 
 function main() {
+	$.get("/get/username")
+		.done(_ => window.location.href = "/home.html")
 	$("form").submit(formSubmitHandler);
 	$("#notifArea").click(function() { $(this).empty() })
+	$("#createGuestGame").click(e => 
+		$.post({
+			url: "/games/create",
+			data: $.param({ cpu: 1 }),
+			success: res => window.location.href = `/play.html?gid=${res}`,
+			error: err => {
+				console.error(err)
+				alert("Encountered an error in setting up a guest game")
+			}
+		})
+	)
 	
 	if (unauthorizedKeyPresent()) {
 		$("<p/>")
 			.text("Unauthorized! Your session may have expired")
 			.appendTo("#notifArea");
 	}
-	$.get("/get/username")
-		.done(_ => window.location.href = "/home.html")
 }
 
 $(main);
